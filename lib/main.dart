74e64aa9d5bab381d0bfaadef6bbaa115563fcef
dev5576 <pronay@enzan-k.com>
2021-05-14 13:07:32 +0900

    added restart button

--- lib/main.dart
+++ lib/main.dart
@@ -16,22 +16,46 @@ can only be used MyApp class...class located on other files cannot use it*/
 class _MyAppState extends State<MyApp> {
   //_ syntax private property
   var _questionIndex = 0;
+  var _totalScore = 0;
   final _questions = const [
     {
       'questionText': 'what is your favourite color?',
-      'answers': ['red', 'green', 'yellow']
+      'answers': [
+        {'text': 'black', 'score': 10},
+        {'text': 'red', 'score': 5},
+        {'text': 'green', 'score': 3},
+        {'text': 'white', 'score': 1},
+      ]
     },
     {
       'questionText': 'what is your favourite animal?',
-      'answers': ['deer', 'lion', 'snake']
+      'answers': [
+        {'text': 'deer', 'score': 10},
+        {'text': 'lion', 'score': 5},
+        {'text': 'snake', 'score': 3},
+        {'text': 'bird', 'score': 1},
+      ]
     },
     {
       'questionText': 'what is your favourite car?',
-      'answers': ['bmw', 'ferrari', 'honda']
+      'answers': [
+        {'text': 'bmw', 'score': 10},
+        {'text': 'ferrari', 'score': 5},
+        {'text': 'honda', 'score': 3},
+        {'text': 'plane', 'score': 1},
+      ]
     },
   ];
 
-  void _AnswerQuestion() {
+  void _resetQuiz() {
+    setState(() {
+      _totalScore = 0;
+      _questionIndex = 0;
+    });
+  }
+
+  void _AnswerQuestion(int score) {
+    _totalScore = _totalScore + score;
     setState(() {
       print('setState()  called!');
       _questionIndex = (_questionIndex + 1);
@@ -50,7 +74,7 @@ class _MyAppState extends State<MyApp> {
               answerQuestion: _AnswerQuestion,
               questionIndex: _questionIndex,
               questions: _questions)
-          : Result(),
+          : Result(_totalScore, _resetQuiz),
     ));
   }
 }
--- lib/result.dart
+++ lib/result.dart
@@ -1,12 +1,41 @@
 import 'package:flutter/material.dart';
 
 class Result extends StatelessWidget {
-  const Result({Key key}) : super(key: key);
+  final int resultScore;
+  final Function resetHandler;
+
+  Result(this.resultScore, this.resetHandler);
+
+  String get ResultPhrase {
+    String resultText;
+    if (resultScore <= 8) {
+      resultText = 'You did it ' + resultScore.toString();
+    } else if (resultScore <= 12) {
+      resultText = 'You did it ' + resultScore.toString();
+    } else {
+      resultText = 'You did it ';
+    }
+    return resultText;
+  }
 
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text('You did it'),
+      child: Column(
+        children: [
+          Text(
+            ResultPhrase,
+            style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+            textAlign: TextAlign.center,
+          ),
+          FlatButton(
+            onPressed: resetHandler,
+            child: Text('Restart Quiz!',
+                style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold)),
+            textColor: Colors.blue,
+          ),
+        ],
+      ),
     );
   }
 }
